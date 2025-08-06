105462120c1eea0b9aa99d50df8488ae13dd34ae
skycode9 <skycode9005@gmail.com>
2025-08-05 19:43:58 -0600

    in this how to update the context value

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,18 +1,31 @@
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
+import UserContext from "../utils/UserContext";
 
 const MainLayout = () => {
+  const [userName, setUserName] = useState("null");
+  //Make a API Call for authentication
+  useEffect(() => {
+    //call api and received a logged in data
+    const data = {
+      name: "Akash Amin",
+    };
+    setUserName(data.name);
+  }, []);
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="container mx-auto px-3 flex-grow">
-        <Header />
-        <main>
-          <Outlet />
-        </main>
+    <UserContext.Provider value={{ loggedInUser: userName }}>
+      <div className="min-h-screen flex flex-col">
+        <div className="container mx-auto px-3 flex-grow">
+          <Header />
+          <main>
+            <Outlet />
+          </main>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </UserContext.Provider>
   );
 };
 

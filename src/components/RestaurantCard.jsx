424d6f47cc7d3066b4dc432c8cc294b29b9f867c
skycode9 <skycode9005@gmail.com>
2025-08-14 14:52:09 -0600

    style: improve restaurant card layout with fixed height and text truncation

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -6,15 +6,15 @@ const RestaurantCard = ({ restData }) => {
 
   return (
     <>
-      <div className="w-[250px] border border-black p-[5px] bg-gray-100 m-5 cursor-pointer hover:bg-gray-200 mb-4">
+      <div className="w-[250px] h-[420px] border border-black p-[5px] bg-gray-100 m-5 cursor-pointer hover:bg-gray-200 mb-4 flex flex-col">
         <img
           src={IMAGE_BASE_URL + cloudinaryImageId}
           alt=""
-          className="rounded mb-4"
+          className="rounded mb-4 h-[180px] w-full object-cover"
         />
-        <div className="flex flex-col gap-3">
-          <h3 className="font-semibold text-xl">{name}</h3>
-          <h4>{cuisines.join(", ") || cuisines[0]}</h4>
+        <div className="flex flex-col gap-3 flex-grow">
+          <h3 className="font-semibold text-xl line-clamp-2">{name}</h3>
+          <h4 className="line-clamp-2">{cuisines.join(", ") || cuisines[0]}</h4>
           <h4>{avgRating} star</h4>
           <h4>{costForTwo}</h4>
           <h4>{sla?.slaString}</h4>

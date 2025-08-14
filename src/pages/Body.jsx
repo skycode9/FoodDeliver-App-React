2ed7f9ab4b113a5e52fe54c90e98125e28533878
skycode9 <skycode9005@gmail.com>
2025-08-14 14:57:51 -0600

    feat: configure proxy to handle Swiggy API requests through local server

--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -34,7 +34,7 @@ const Body = () => {
       setIsLoading(true);
 
       const data = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "/api/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
       const json = await data.json();
       // Optional Chaining
--- src/utils/comman.js
+++ src/utils/comman.js
@@ -5,4 +5,4 @@ export const LOGO_URL =
   "https://png.pngtree.com/png-clipart/20200727/original/pngtree-food-delivery-logo-design-png-image_5392526.jpg";
 
 export const MENU_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0759837&lng=72.8776559&restaurantId=";
+  "/api/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0759837&lng=72.8776559&restaurantId=";

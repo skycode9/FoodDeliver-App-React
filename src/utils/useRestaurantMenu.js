2a53f54bdcc501aec07a0a01a38a65bdde2502b3
skycode9 <skycode9005@gmail.com>
2025-10-31 11:50:17 -0600

    fix: correct API fetch URL by adding restaurantId query parameter

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -10,7 +10,7 @@ const useRestaurantMenu = (restId) => {
       try {
         console.log("ResURL", MENU_URL + restId);
 
-        const data = await fetch(`/api/menu` + restId);
+        const data = await fetch(`/api/menu?restaurantId=` + restId);
 
         console.log("data", data);
 
